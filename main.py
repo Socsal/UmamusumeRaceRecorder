d0b81348897ea7e5ecfc1766219b68fd3577a0ae
Socsal <321054237@qq.com>
2025-12-25 01:46:51 +0800

    init project

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import subprocess
+import time
+import cv2
+import numpy as np
+from PIL import Image
+import pytesseract
+from datetime import datetime
+import argparse
+import re
